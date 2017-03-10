b138212b087240330c57e56556124461b3c3ef5c
patriot <patriot130690@gmail.com>
2017-03-10 23:57:34 +0200

    UI elements added

--- RetroLines/Assets/Scripts/SetField.cs
+++ RetroLines/Assets/Scripts/SetField.cs
@@ -1,4 +1,20 @@
-﻿using System.Collections;
+﻿/*  Retro Lines. Lines 98 clone done on unity.
+    Copyright (C) 2017  Alexander Triukhan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -21,10 +37,6 @@ public class SetField : MonoBehaviour {
 		instantiateTiles();
 	}
 
-	void Update () {
-		
-	}
-
 	// ... ... ...
 	// 2,0 2,1 ...
 	// 1,0 1,1 ...
@@ -43,11 +55,11 @@ public class SetField : MonoBehaviour {
 		}
 	}
 
-	private void instantiateTiles(){
+	private void instantiateTiles() {
 		int centerWidth = ((Screen.width / 9) / 2);
 		for (int i = 0; i < gridWidth; i++) {
 			for (int j = 0; j < gridHeigth; j++) {
-				RawImage tile = Instantiate(tileSprite, canvas) as RawImage;
+				RawImage tile = Instantiate(tileSprite, canvas.transform) as RawImage;
 				tile.rectTransform.anchoredPosition3D = new Vector3(gridPos [i, j].x, gridPos [i, j].y, 0);
 				tile.rectTransform.localScale = new Vector3(1, 1, 1);
 				tile.rectTransform.sizeDelta = new Vector2(centerWidth * 2, centerWidth * 2);
---
+++ RetroLines/Assets/Scripts/UIScaler.cs
@@ -0,0 +1,42 @@
+﻿/*  Retro Lines. Lines 98 clone done on unity.
+    Copyright (C) 2017  Alexander Triukhan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScaler : MonoBehaviour {
+
+	public Text ScoreText;
+	public Text BallsText;
+	private const int offsetDivider = 10;
+	private const float offsetMultiplier = 1.6f;
+	private const int minScreenWidth = 240;
+
+	void Start () {
+		SetScale();
+		ScoreText.text = "00000";
+		BallsText.text = "00000";
+	}
+		
+	private void SetScale() {
+		float scaleMultiplier = (float)Screen.width / (float)minScreenWidth;
+		ScoreText.rectTransform.localScale = new Vector2(scaleMultiplier, scaleMultiplier);
+		BallsText.rectTransform.localScale = new Vector2(scaleMultiplier, scaleMultiplier);
+		BallsText.rectTransform.anchoredPosition = new Vector2(-(Screen.width / 4),0);
+	}
+}
